8c17a7bedde02824ffb3154d2ca28623aa43a1fb
Antonio García Domínguez <antonio.garciadominguez@uca.es>
2014-09-26 22:45:54 +0200

    Actualizado TopoJSON a partir del último fichero de Alberto

--- datos/generar_topojson.sh
+++ datos/generar_topojson.sh
@@ -17,7 +17,7 @@ rm -f aragon.geojson provincias.geojson comarcas.geojson municipios.geojson
 
 geojson T02_Municipios.prj aragon.geojson aragon.shp
 geojson T02_Municipios.prj provincias.geojson provincias.shp
-geojson T02_Comarcas.prj comarcas.geojson T02_Comarcas.shp
+zcat t02_comarcas.geojson.gz > comarcas.geojson
 geojson T02_Municipios.prj municipios.geojson T02_Municipios.shp
 
 topojson aragon.topojson \
