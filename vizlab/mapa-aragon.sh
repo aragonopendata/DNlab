654f402d575ea7c581724e2eecb6ae425150b715
Antonio García Domínguez <antonio.garciadominguez@uca.es>
2014-09-26 16:37:45 +0200

    Recolocado ficheros

--- vizlab/mapa-aragon.sh
+++ vizlab/mapa-aragon.sh
@@ -3,4 +3,4 @@
 mkdir -p json
 zcat ../datos/municipios.topojson.gz > json/municipios.topojson
 python -m SimpleHTTPServer 8888 &
-xdg-open http://localhost:8888/topojson.html
+xdg-open http://localhost:8888/mapa-aragon.html
