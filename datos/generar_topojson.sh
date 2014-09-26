8a96c163ebce2028a8970acc43f3b4245af9a1bf
Antonio García Domínguez <antonio.garciadominguez@uca.es>
2014-09-26 19:45:11 +0200

    Añadido guión para generar el .topojson

---
+++ datos/generar_topojson.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+geojson() {
+  ogr2ogr -f GeoJSON -s_srs "$1" -t_srs EPSG:4326 "$2" "$3"
+}
+
+topojson() {
+  TOPOJSON="$1"
+  shift
+  ../node_modules/topojson/bin/topojson -p -o "$TOPOJSON" -- $@
+}
+
+unzip -o municipios.aragon.shp.zip
+unzip -o municipios_comarcas.shp.zip
+
+rm -f aragon.geojson provincias.geojson comarcas.geojson municipios.geojson
+
+geojson T02_Municipios.prj aragon.geojson aragon.shp
+geojson T02_Municipios.prj provincias.geojson provincias.shp
+geojson T02_Comarcas.prj comarcas.geojson T02_Comarcas.shp
+geojson T02_Municipios.prj municipios.geojson T02_Municipios.shp
+
+topojson aragon.topojson \
+  aragon.geojson provincias.geojson comarcas.geojson municipios.geojson
+
+gzip -9 aragon.topojson
