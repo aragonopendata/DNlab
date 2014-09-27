0debc18b98e6af0557f9137892454c672b7ff386
Antonio García Domínguez <antonio.garciadominguez@uca.es>
2014-09-27 13:22:40 +0200

    Creado visualización de comarca a población + estudiantes por tipo

--- vizlab/js/mapa-vinculado.js
+++ vizlab/js/mapa-vinculado.js
@@ -1,55 +1,154 @@
 (function(){
 
+    // http://stackoverflow.com/questions/4833651/
+    function sort_unique(arr) {
+	arr = arr.sort(function (a, b) { return a*1 - b*1; });
+	var ret = [arr[0]];
+	for (var i = 1; i < arr.length; i++) { // start loop at 1 as element 0 can never be a duplicate
+            if (arr[i-1] !== arr[i]) {
+		ret.push(arr[i]);
+            }
+	}
+	return ret;
+    }
+
+    function GraficaPob(json_pob) {
+	var self = {};
+
+	var colNac;
+	var colExt;
+	var anyos;
+	self.actualizarDatosComarca = function(codComarca) {
+	    var datosComarca = json_pob
+		.filter(function (e) { return e.c_comarca == codComarca; })
+		.sort(function (a, b) { return parseInt(a.anyo) - parseInt(b.anyo); });
+
+	    anyos = datosComarca.map(function (e) { return e.anyo; });
+	    colNac = ['Nacionales'].concat(datosComarca.map(function (e) { return e.pob_nac_i; }));
+	    colExt = ['Extranjeros'].concat(datosComarca.map(function (e) { return e.pob_ext_i; }));
+	}
+
+	var chart;
+	self.crearGrafica = function() {
+	    chart = c3.generate({
+		bindto: "#crecPoblacion",
+		size: {height: 300, width: 400},
+		data: {
+		    columns: [colNac, colExt],
+		    types: {Nacionales: 'area-spline', Extranjeros: 'area-spline'},
+		    groups: [[colNac[0], colExt[0]]],
+		},
+		axis: {
+		    x: {label: 'Año', type: 'category', categories: anyos},
+		    y: {label: 'Habitantes'},
+		},
+	    });
+	}
+
+	self.actualizarGrafica = function(codComarca) {
+	    self.actualizarDatosComarca(codComarca);
+	    chart.load({
+		columns: [colNac, colExt],
+	    });
+	}
+
+	return self;
+    }
+
+
+
+    function GraficaAlum(json_alum) {
+	var self = {};
+
+	var colPub, colPriC, colPriNC;
+	var anyos;
+	self.actualizarDatosComarca = function(codComarca) {
+	    // Filtrar por comarca y ordenar por año
+	    var datosComarca = json_alum
+		.filter(function (e) { return e.codigo == codComarca; })
+		.sort(function (a, b) { return parseInt(a.anyo) - parseInt(b.anyo); });
+
+	    anyos = sort_unique(datosComarca.map(function (e) { return parseInt(e.anyo); }));
+	    datosFiltrados = {};
+	    for (var i = 0; i < anyos.length; i++) {
+		datosFiltrados[anyos[i]] = {pub: 0, priC: 0, priNC: 0};
+	    }
+	    for (var i = 0; i < datosComarca.length; i++) {
+		var d = datosComarca[i];
+		var y = d.anyo;
+		var n = d.alumnado_i;
+
+		if (!y) {
+		    continue;
+		}
+
+		if ('Pública' == d.titularidad) {
+		    datosFiltrados[y].pub = n;
+		}
+		else if ('Privado concertado' == d.titularidad) {
+		    datosFiltrados[y].priC = n;
+		}
+		else {
+		    datosFiltrados[y].priNC = n;
+		}
+	    }
+
+	    colPub = ['Pública'].concat(anyos.map(function (y) { return datosFiltrados[y].pub; }));
+	    colPriC = ['Privada conc'].concat(anyos.map(function (y) { return datosFiltrados[y].priC; }));
+	    colPriNC = ['Privada no conc'].concat(anyos.map(function (y) { return datosFiltrados[y].priNC; }));
+	}
+
+	var chart;
+	self.crearGrafica = function() {
+	    chart = c3.generate({
+		bindto: "#alumnado",
+		size: {height: 300, width: 400},
+		data: {
+		    columns: [colPub, colPriNC, colPriC],
+		    types: {Pública: 'area-spline', 'Privada no conc': 'area-spline', 'Privada conc': 'area-spline'},
+		    groups: [[colPub[0], colPriNC[0], colPriC[0]]],
+		},
+		axis: {
+		    x: {label: 'Año', type: 'category', categories: anyos},
+		    y: {label: 'Estudiantes'},
+		},
+	    });
+	}
+
+	self.actualizarGrafica = function(codComarca) {
+	    self.actualizarDatosComarca(codComarca);
+	    chart.load({
+		columns: [colPub, colPriNC, colPriC],
+	    });
+	}
+
+	return self;
+    }
+
     d3.json(
 	"json/crecimiento_poblacion_comarca.json",
 	function (error, json_pob) {
 	    if (error) return console.log("Error cargando poblaciones");
 
-	    var colNac;
-	    var colExt;
-	    var anyos;
-	    function actualizarDatosComarca(codComarca) {
-		var datosComarca = json_pob
-		    .filter(function (e) { return e.c_comarca == codComarca; })
-		    .sort(function (a, b) { return parseInt(a.anyo) - parseInt(b.anyo); });
+	    d3.json("json/alumnado_por_comarcas.json", function (error, json_alum) {
+		if (error) return console.log("Error cargando alumnos");
 
-		anyos = datosComarca.map(function (e) { return e.anyo; });
-		colNac = ['Nacionales'].concat(datosComarca.map(function (e) { return e.pob_nac_i; }));
-		colExt = ['Extranjeros'].concat(datosComarca.map(function (e) { return e.pob_ext_i; }));
-	    }
+		MapaAragon.dibujarMapas(function (e) {
+		    var gp = GraficaPob(json_pob);
+		    var ga = GraficaAlum(json_alum);
 
-	    var chart;
-	    function crearGrafica() {
-		// Filtramos los datos de una comarca concreta
-		chart = c3.generate({
-		    bindto: "#crecPoblacion",
-		    size: {height: 300, width: 400},
-		    data: {
-			columns: [colNac, colExt],
-			types: {Nacionales: 'area-spline', Extranjeros: 'area-spline'},
-			groups: [[colNac[0], colExt[0]]],
-		    },
-		    axis: {
-			x: {label: 'Año', type: 'category', categories: anyos},
-			y: {label: 'Habitantes'},
-		    },
-		});
-	    }
+		    gp.actualizarDatosComarca(17);
+		    ga.actualizarDatosComarca(17);
+		    gp.crearGrafica();
+		    ga.crearGrafica();
 
-	    function actualizarGrafica(codComarca) {
-		actualizarDatosComarca(codComarca);
-		chart.load({
-		    columns: [colNac, colExt],
-		});
-	    }
-
-	    MapaAragon.dibujarMapas(function (e) {
-		actualizarDatosComarca(17);
-		crearGrafica();
-		e.svg.selectAll(".comarcas path").on("click", function (d) {
-		    actualizarGrafica(d.properties.c_comarca);
+		    e.svg.selectAll(".comarcas path").on("click", function (d) {
+			gp.actualizarGrafica(d.properties.c_comarca);
+			ga.actualizarGrafica(d.properties.c_comarca);
+		    });
 		});
 	    });
+
 	}
     );
 })();
--- vizlab/mapa-vinculado.sh
+++ vizlab/mapa-vinculado.sh
@@ -4,5 +4,6 @@ mkdir -p json
 zcat ../datos/aragon.topojson.gz > json/aragon.topojson
 cp ../datos/geocodes/colegios.json json
 cp ../datos/crecimiento_poblacion_comarca.json json
+cp ../datos/alumnado_por_comarcas.json json
 python -m SimpleHTTPServer 8888 &
 xdg-open http://localhost:8888/mapa-vinculado.html
