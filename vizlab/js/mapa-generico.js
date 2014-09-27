00ba922d0dec4f16e94ea385be8f69b7ae24e82e
Antonio García Domínguez <antonio.garciadominguez@uca.es>
2014-09-27 10:24:23 +0200

    Creado .js reutilizable para crear mapas sencillos estáticos de Aragón

---
+++ vizlab/js/mapa-generico.js
@@ -0,0 +1,110 @@
+(function() {
+
+    function arrayToObject(a) {
+	var o = {};
+	for (var i = 0; i < a.length; i++) {
+	    o[a[i]] = 1;
+	}
+	return o;
+    }
+
+    function dibujarMapa(elem, json_aragon, json_colegios) {
+	var w = parseInt(elem.attr("data-w")) || 400, h = parseInt(elem.attr("data-h")) || 400;
+	var svg = elem.append("svg").attr({width: w, height: h});
+
+	// Cargamos las partes del mapa TopoJSON
+	var aragon = topojson.feature(json_aragon, json_aragon.objects.aragon);
+	var provincias = topojson.feature(json_aragon, json_aragon.objects.provincias);
+	var comarcas = topojson.feature(json_aragon, json_aragon.objects.comarcas);
+	var municipios = topojson.feature(json_aragon, json_aragon.objects.municipios);
+
+	// Calculamos el enfoque
+	var enfoque = aragon;
+	var provincia = elem.attr("data-provincia");
+	var codcomarca = elem.attr("data-codcomarca");
+	if (provincia) {
+	    provincias.features = provincias.features.filter(function (e) { return e.properties.PROVINCIA == provincia; });
+	    comarcas.features = comarcas.features.filter(function (e) { return e.properties.provincia == provincia; });
+	    municipios.features = municipios.features.filter(function (e) { return e.properties.PROVINCIA == provincia; });
+
+	    json_colegios = json_colegios.filter(function (e) { return e.provincia == provincia; });
+
+	    enfoque = provincias.features[0];
+	}
+	if (codcomarca) {
+	    comarcas.features = comarcas.features.filter(function (e) { return e.properties.c_comarca == codcomarca; });
+	    municipios.features = municipios.features.filter(function (e) { return e.properties.C_COMARCA == codcomarca; });
+	    enfoque = comarcas.features[0];
+	}
+
+	// De http://stackoverflow.com/questions/14492284/ //////////
+
+	// Creamos una primera proyección centrada
+	var scale  = 1;
+	var offset = [0, 0];
+	var projection = d3.geo.mercator().scale(scale).translate(offset);
+	var path = d3.geo.path().projection(projection);
+
+	// Compute the bounds of a feature of interest, then derive scale & translate.
+	var b = path.bounds(enfoque),
+	s = .95 / Math.max((b[1][0] - b[0][0]) / w, (b[1][1] - b[0][1]) / h),
+	t = [(w - s * (b[1][0] + b[0][0])) / 2, (h - s * (b[1][1] + b[0][1])) / 2];
+
+	// Update the projection to use computed scale & translate.
+	projection.scale(s).translate(t);
+
+	//////////////////////////////////////////////////
+
+	var sCapasSel = elem.attr("data-capas") || "comunidad";
+	var capasSel = arrayToObject(sCapasSel.split(","));
+	var capas = [];
+	if ("municipios" in capasSel) capas.push(["municipios", municipios]);
+	if ("provincias" in capasSel) capas.push(["provincias", provincias]);
+	if ("comunidad" in capasSel) capas.push(["aragon", aragon]);
+	if ("comarcas" in capasSel) capas.push(["comarcas", comarcas]);
+
+	// Dibujamos las capas principales
+	for (var i = 0; i < capas.length; i++) {
+	    var clase = capas[i][0];
+	    var datos = capas[i][1];
+
+	    // Dibujamos las provincias
+	    var g = svg.append("g").attr("class", clase);
+	    g.selectAll("path")
+		.data(datos.features)
+		.enter()
+		.append("path")
+		.attr("d", path)
+		.on("click", function (d) { console.log(d); });
+	}
+
+	// Dibujamos los colegios
+	if ("colegios" in capasSel) {
+	    var g = svg.append("g").attr("class", "colegios");
+
+	    g.selectAll("circle")
+		.data(json_colegios)
+		.enter()
+		.append("circle").attr({
+		    "transform": function (d) { return "translate(" + projection([d["lng"], d["lat"]]) + ")"; },
+		    "r": 2,
+		})
+		.on("click", function (d) { console.log(d); })
+	    ;
+	}
+    }
+
+    d3.json("json/aragon.topojson", function (error, json_aragon) {
+	if (error) return console.log('Error cargando TopoJSON', error);
+
+	d3.json("json/colegios.json", function (error, json_colegios) {
+	    if (error) return console.log('Error cargando colegios', error);
+
+	    var mapas = d3.selectAll(".mapa")[0];
+	    for (var i = 0; i < mapas.length; i++) {
+		dibujarMapa(d3.select(mapas[i]), json_aragon, json_colegios);
+	    }
+	});
+    });
+
+})();
