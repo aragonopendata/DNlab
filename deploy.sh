ebdc9640887cf0a94af2bce716f02dc46cf438fc
Antonio García Domínguez <antonio.garciadominguez@uca.es>
2014-09-27 14:19:30 +0200

    Retocado guión rsync para subir las otras visualizaciones

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,13 @@
 
 IP=155.210.71.92
 
-rsync --exclude '#*' --exclude '*~' -rvz vizlab JacatonUser@$IP:/var/www/jacaton/
+subir() {
+    rsync --exclude '#*' --exclude '*~' -rvz "$1" JacatonUser@$IP:/var/www/jacaton/
+}
+
+subir vizlab
+subir crossfilter
+subir micromaps
+
+
+
